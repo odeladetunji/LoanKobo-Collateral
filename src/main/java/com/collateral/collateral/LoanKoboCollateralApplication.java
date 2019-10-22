0e06d984fb45fe7896ea25e390789c644a6b34fb
Olatunji Odelade <odeladetunji@gmail.com>
2019-10-22 15:24:42 +0100

    first commit

---
+++ src/main/java/com/collateral/collateral/LoanKoboCollateralApplication.java
@@ -0,0 +1,13 @@
+package com.collateral.collateral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanKoboCollateralApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoanKoboCollateralApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/collateral/collateral/LoanKoboCollateralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collateral.collateral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanKoboCollateralApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
